0860b95f2fe606cfd9cb2daefe5b68e7d6609473
MOSW626 <ays6533@naver.com>
2025-11-24 20:39:55 +0900

    Update Arduino Nano code to modify pin assignments and enhance fan and peltier control logic

--- chair/arduino_nano/arduino_nano.ino
+++ chair/arduino_nano/arduino_nano.ino
@@ -1,35 +1,50 @@
-#define FAN_top 5
-#define FAN_1 6
-#define FAN_2 7
-#define peltier 8
+#define FAN_top 3
+#define FAN_1 4
+#define FAN_2 5
+#define pel1 6
+#define pel2 7
+#define pel3 8
+#define pel4 9
 
 void setup()
 {
   pinMode(FAN_top, OUTPUT);
   pinMode(FAN_1, OUTPUT);
   pinMode(FAN_2, OUTPUT);
-  pinMode(peltier, OUTPUT);
+  pinMode(pel1, OUTPUT);
+  pinMode(pel2, OUTPUT);
+  pinMode(pel3, OUTPUT);
+  pinMode(pel4, OUTPUT);
+
+  digitalWrite(FAN_top, LOW);
+  digitalWrite(FAN_1, LOW);
+  digitalWrite(FAN_2, LOW);
+  digitalWrite(pel1, HIGH);
+  digitalWrite(pel2, HIGH);
+  digitalWrite(pel3, HIGH);
+  digitalWrite(pel4, HIGH);
 
   Serial.begin(9600);
 }
 
 void loop()
 {
-  int val = analogRead(A5)/4;
-  Serial.println(val);
-
+  int val = analogRead(A1)/4;
+  analogWrite(FAN_top, val);
+  analogWrite(FAN_1, val);
+  analogWrite(FAN_2, val);
   if(val < 20)
   {
-    digitalWrite(FAN_top, LOW);
-    digitalWrite(FAN_1, LOW);
-    digitalWrite(FAN_2, LOW);
-    digitalWrite(peltier, LOW);
+    digitalWrite(pel1, HIGH);
+    digitalWrite(pel2, HIGH);
+    digitalWrite(pel3, HIGH);
+    digitalWrite(pel4, HIGH);
   }
   else
   {
-    analogWrite(FAN_top, val);
-    analogWrite(FAN_1, val);
-    analogWrite(FAN_2, val);
-    analogWrite(peltier, val);
+    analogWrite(pel1, LOW);
+    analogWrite(pel2, LOW);
+    analogWrite(pel3, LOW);
+    analogWrite(pel4, LOW);
   }
 }
